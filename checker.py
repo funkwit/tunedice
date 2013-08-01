6479d83bfa6fa9a14ea52906005a89cd82942a82
Paul Cowan <paul@custardsource.com>
2013-08-01 13:47:46 +1000

    Split walker code from checker cli

--- checker.py
+++ checker.py
@@ -1,40 +1,9 @@
 #!/usr/bin/python
 
 from optparse import OptionParser
+import walker
 
 import sys, os, fnmatch, re, glob
-
-class Checker(object):
-  def missing(self, directory, user, metafile):
-    for miss in self.check_directory(directory, user, metafile, missing=True):
-      yield miss
-
-  def check_directory(self, path, user, metafile, missing):
-    if not os.path.isdir(path):
-      return
-    meta = os.path.join(path, metafile)
-    try:
-      with open(meta) as f:
-        for line in f.readlines():
-          if line.strip() == ("!" + user):
-            return
-          if line.strip() == user:
-            if missing:
-              return
-            else:
-              yield path
-    except IOError:
-      pass
-
-    if glob.glob(os.path.join(path, "*.mp3")):
-      if missing:
-        yield path
-    else:
-      for dirpath in sorted(os.listdir(path)):
-        for miss in self.check_directory(os.path.join(path, dirpath), user,
-                                         metafile, missing):
-          yield miss
-
 
 def docheck():
   parser = OptionParser()
@@ -45,9 +14,9 @@ def docheck():
   (options, args) = parser.parse_args()
   if not args:
     raise SystemExit(parser.print_help() or 1)
-  checker = Checker()
-  for miss in checker.missing(args[0], user=options.user,
-                              metafile=options.meta_file):
+  walk = walker.Walker()
+  for miss in walk.missing(args[0], user=options.user,
+                           metafile=options.meta_file):
     print "Missing: " + miss
 
 
---
+++ walker.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+import os, glob
+
+class Walker(object):
+  def missing(self, directory, user, metafile):
+    for miss in self.check_directory(directory, user, metafile, missing=True):
+      yield miss
+
+  def check_directory(self, path, user, metafile, missing):
+    if not os.path.isdir(path):
+      return
+    meta = os.path.join(path, metafile)
+    try:
+      with open(meta) as f:
+        for line in f.readlines():
+          if line.strip() == ("!" + user):
+            return
+          if line.strip() == user:
+            if missing:
+              return
+            else:
+              yield path
+    except IOError:
+      pass
+
+    if glob.glob(os.path.join(path, "*.mp3")):
+      if missing:
+        yield path
+    else:
+      for dirpath in sorted(os.listdir(path)):
+        for miss in self.check_directory(os.path.join(path, dirpath), user,
+                                         metafile, missing):
+          yield miss
